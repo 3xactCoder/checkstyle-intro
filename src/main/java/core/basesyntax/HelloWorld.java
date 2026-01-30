8deddf79471c213b9959b61672366c595a1b13e8
3xacT <vovaoliynyk99@gmail.com>
2026-01-30 18:42:54 +0200

    fixed error which occurs after pr

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,13 +1,12 @@
 package core.basesyntax;
 
-import core.basesyntax.Model.Dog;
-import core.basesyntax.Model.Cat;
-
+import core.basesyntax.model.Cat;
+import core.basesyntax.model.Dog;
 import java.time.LocalDate;
 
+public class Hello_World {
 
-    public class Hello_World {
-    private String a;
+    private String abl;
     private int variable;
     private int age;
     private int size;
@@ -29,16 +28,16 @@ import java.time.LocalDate;
         return result;
     }
 
-    private void sayHello(String Hello) {
-        System.out.println("Hello, Mate! I am happy to see you! This class looks weird!" +
-                " Your task is to fix the checkstyle in this file." +
-                " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
-        System.out.println(Hello);
+    private void sayHello(String hello) {
+        System.out.println("Hello, Mate! I am happy to see you! This class looks weird!"
+                + " Your task is to fix the checkstyle in this file."
+                + " Your goal is to run `mvn clean package` and receive `BUILD SUCCESS` message");
+        System.out.println(hello);
     }
 
     private void initializeVariables() {
-        a = "Hello mates!";
-        System.out.println(a);
+        abl = "Hello mates!";
+        System.out.println(abl);
         variable = LocalDate.now().getYear();
         System.out.println("It is " + variable + "year");
         age = 25;
